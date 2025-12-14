7dae96779b85d93ee7e50fef98eb3b4aeeff5c62
RxSajib <sajibroy206@gmail.com>
2025-12-14 21:54:35 +0600

    sign up account ui design

--- composeApp/src/commonMain/kotlin/org/example/project/screen/signupScreen/SignUpScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/screen/signupScreen/SignUpScreen.kt
@@ -1,9 +1,15 @@
 package org.example.project.ui.screen.signupScreen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -11,30 +17,46 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.sajib.presentation.ui.signupScreen.SignUpViewModel
 import org.example.project.component.BackButton
 import org.example.project.component.HeightGap
+import org.example.project.component.MyCustomButton
 import org.example.project.component.MyCustomInputFiled
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 import travelapp.composeapp.generated.resources.Res
+import travelapp.composeapp.generated.resources.already_have_account
+import travelapp.composeapp.generated.resources.confirm_password
+import travelapp.composeapp.generated.resources.dont_have_an_account
 import travelapp.composeapp.generated.resources.enter_email
+import travelapp.composeapp.generated.resources.enter_name
+import travelapp.composeapp.generated.resources.enter_password
+import travelapp.composeapp.generated.resources.sign_in
+import travelapp.composeapp.generated.resources.sign_in_account
 import travelapp.composeapp.generated.resources.sign_in_now_details
+import travelapp.composeapp.generated.resources.sign_up
+import travelapp.composeapp.generated.resources.sign_up_account
 import travelapp.composeapp.generated.resources.sign_up_now
 import travelapp.composeapp.generated.resources.sign_up_now_details
 
 @Composable
-fun SignUpScreen(){
+fun SignUpScreen() {
 
-    val viewModel : SignUpViewModel = koinViewModel()
+    val viewModel: SignUpViewModel = koinViewModel()
 
     Scaffold { innerPadding ->
-        Column(modifier = Modifier.fillMaxSize().padding(innerPadding).padding(16.dp)) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding).verticalScroll(
+                rememberScrollState()
+            ).padding(16.dp)
+        ) {
             BackButton(imageVector = Icons.AutoMirrored.Default.ArrowBack, onclick = {})
             HeightGap(height = 20.dp)
             Text(
@@ -53,6 +75,17 @@ fun SignUpScreen(){
             )
             HeightGap(height = 20.dp)
             MyCustomInputFiled(
+                placeHolderText = stringResource(Res.string.enter_name),
+                text = viewModel.nameInput,
+                onValueChange = { nameInput ->
+                    viewModel.nameInput = nameInput
+                },
+                isPasswordInput = false,
+                isVisiblePasswordChange = {},
+                isPasswordVisibility = true
+            )
+            HeightGap(height = 10.dp)
+            MyCustomInputFiled(
                 placeHolderText = stringResource(Res.string.enter_email),
                 text = viewModel.nameInput,
                 onValueChange = { nameInput ->
@@ -63,16 +96,50 @@ fun SignUpScreen(){
                 isPasswordVisibility = true
             )
             HeightGap(height = 10.dp)
-
-            Button(onClick = {
-                viewModel.signUpAccount(
-                    name = "Sajib Roy",
-                    emailAddress = "sabr@gmail.com",
-                    password = "123456",
-                    confirmPassword = "123456"
+            MyCustomInputFiled(
+                placeHolderText = stringResource(Res.string.enter_password),
+                text = viewModel.nameInput,
+                onValueChange = { nameInput ->
+                    viewModel.nameInput = nameInput
+                },
+                isPasswordInput = false,
+                isVisiblePasswordChange = {},
+                isPasswordVisibility = true
+            )
+            HeightGap(height = 10.dp)
+            MyCustomInputFiled(
+                placeHolderText = stringResource(Res.string.confirm_password),
+                text = viewModel.nameInput,
+                onValueChange = { nameInput ->
+                    viewModel.nameInput = nameInput
+                },
+                isPasswordInput = false,
+                isVisiblePasswordChange = {},
+                isPasswordVisibility = true
+            )
+            HeightGap(height = 20.dp)
+            MyCustomButton(
+                title = stringResource(Res.string.sign_up_account),
+                modifier = Modifier,
+                onClickButton = {},
+                isEnable = true
+            )
+            HeightGap(height = 20.dp)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(Res.string.already_have_account),
+                    style = MaterialTheme.typography.bodySmall
                 )
-            }){
-                Text("Login Account")
+                Text(
+                    text = stringResource(Res.string.sign_in),
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.W500,
+                        color = Color.Red,
+                    ), modifier = Modifier.clickable {})
             }
         }
     }
@@ -80,6 +147,6 @@ fun SignUpScreen(){
 
 @Composable
 @Preview(showBackground = true)
-fun SignUpScreenPreview(){
+fun SignUpScreenPreview() {
     SignUpScreen()
 }
